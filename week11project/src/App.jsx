4ec7c529b9b1da4649d213b541c377109073daa9
Laura <laura.hoang05@gmail.com>
2025-12-04 20:56:12 -0600

    Update App.jsx

--- week11project/src/App.jsx
+++ week11project/src/App.jsx
@@ -1,8 +1,8 @@
-import Header from "./src/components/Header.jsx";
-import FeaturedItem from "./src/components/FeaturedItem.jsx";
-import ItemList from "./src/components/ItemList.jsx";
-import FunFact from "./src/components/FunFact.jsx";
-import Footer from "./src/components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import FeaturedItem from "./components/FeaturedItem.jsx";
+import ItemList from "./components/ItemList.jsx";
+import FunFact from "./components/FunFact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const root = document.getElementById("app");
 root.innerHTML = `
