cc29d80af4f3357e5ea170c55d68915c1bde8f34
Laura <laura.hoang05@gmail.com>
2025-12-07 22:34:16 -0600

    hehe

--- week11project/src/components/ItemCard.jsx
+++ week11project/src/components/ItemCard.jsx
@@ -2,7 +2,7 @@ export default function ItemCard(item){
   return `
     <div class ="card"
       onclick= "alert('You clicked on ${item.title}!')"
-      onmouseover= "this.style.border= '2p solid #90bde6'"
+      onmouseover= "this.style.border='2px solid #90bde6'"
       onmouseout= "this.style.border= 'none'">
       <h3>${item.title}</h3>
       <p>${item.desc}</p>
